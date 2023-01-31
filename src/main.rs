6958969ce033d6dfd7736ccb12bbf5cc1e59c142
te <thrizzle@skiff.com>
2023-01-31 18:40:06 -0500

    ✨ add amount query param

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use actix_web::{get, http, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use cached::proc_macro::cached;
 use env_logger;
-use reqwest::Client;
 use rand::seq::SliceRandom;
+use reqwest::Client;
+use serde::Deserialize;
 
 mod types;
 
@@ -12,11 +13,16 @@ const DEFAULT_SUBREDDITS: [&str; 6] = [
     "funny",
     "antimeme",
     "wholesomememes",
-    "me_irl"
+    "me_irl",
 ];
 
 struct AppState {
     client: Client,
+}
+
+#[derive(Deserialize)]
+struct Query {
+    amount: Option<u8>,
 }
 
 #[cached(
@@ -33,41 +39,78 @@ async fn get_memes_from_subreddt(
 ) -> Result<Vec<types::Post>, Box<dyn std::error::Error>> {
     let hot_posts_res = http
         .get(format!("https://reddit.com/r/{}/hot.json", subreddit))
-        .query(&[
-            ("limit", 100),
-        ])
+        .query(&[("limit", 100)])
         .send()
         .await?;
     if hot_posts_res.status() != 200 {
-        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, format!("subreddit r/{} was not found", subreddit))))
+        return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("subreddit r/{} was not found", subreddit),
+        )));
     }
-    let reddit_data: types::RedditResponse = hot_posts_res.json::<types::RedditResponse>().await.unwrap();
-    let memes = reddit_data.data.children.iter().map(|child| child.data.clone()).collect::<Vec<types::Post>>();
+    let reddit_data: types::RedditResponse =
+        hot_posts_res.json::<types::RedditResponse>().await.unwrap();
+    let memes = reddit_data
+        .data
+        .children
+        .iter()
+        .map(|child| child.data.clone())
+        .collect::<Vec<types::Post>>();
     Ok(memes)
 }
 
-async fn get_random_meme(http: &Client, subreddit: String) -> Result<Vec<types::Post>, Box<dyn std::error::Error>> {
+async fn get_random_meme(
+    http: &Client,
+    subreddit: String,
+) -> Result<Vec<types::Post>, Box<dyn std::error::Error>> {
     let meme = get_memes_from_subreddt(http, subreddit).await;
     if meme.is_ok() {
-        return Ok(vec![meme.unwrap().choose(&mut rand::thread_rng()).unwrap().clone()])
+        return Ok(vec![meme
+            .unwrap()
+            .choose(&mut rand::thread_rng())
+            .unwrap()
+            .clone()]);
     } else {
-        return Err(meme.err().unwrap())
+        return Err(meme.err().unwrap());
     }
 }
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, http::Error> {
-    Ok(HttpResponse::Ok().body("Hello World!"))
+async fn index(data: web::Data<AppState>, query: web::Query<Query>) -> impl Responder {
+    let mut memes = Vec::new();
+    for _ in 0..query.amount.unwrap_or(1) {
+        let meme = get_memes_from_subreddt(
+        &data.client,
+        DEFAULT_SUBREDDITS
+            .choose(&mut rand::thread_rng())
+            .unwrap()
+            .clone()
+            .to_string(),
+        )
+        .await;
+        if meme.is_ok() {
+            memes.push(meme.unwrap().choose(&mut rand::thread_rng()).unwrap().clone());
+        }
+    }
+    HttpResponse::Ok().json(
+        memes
+    )
 }
 
-#[get("/{user}")]
-async fn user(data: web::Data<AppState>, subreddit: web::Path<String>) -> impl Responder {
-    let res = get_random_meme(&data.client, subreddit.to_string()).await;
-    if res.is_ok() {
-        return HttpResponse::Ok().json(res.unwrap());
-    } else {
-        return HttpResponse::InternalServerError().body(res.err().expect("Error").to_string());
+#[get("/{subreddit}")]
+async fn get_subreddit(data: web::Data<AppState>, subreddit: web::Path<String>, query: web::Query<Query>) -> impl Responder {
+    let mut memes = Vec::new();
+    for _ in 0..query.amount.unwrap_or(1) {
+        let res = get_random_meme(&data.client, subreddit.to_string()).await;
+        if res.is_ok() {
+            memes.push(res.unwrap().choose(&mut rand::thread_rng()).unwrap().clone());
+        } else {
+            return HttpResponse::InternalServerError().body(res.err().expect("Error").to_string());
+        }
     }
+    HttpResponse::Ok().json(
+        memes
+    )
 }
 
 #[actix_web::main]
@@ -78,7 +121,7 @@ async fn main() -> std::io::Result<()> {
     for subreddit in DEFAULT_SUBREDDITS.iter() {
         println!("[i] fetching memes from r/{}", subreddit.to_string());
         let _ = get_memes_from_subreddt(&temp_client, subreddit.to_string()).await;
-    };
+    }
     drop(temp_client);
     HttpServer::new(|| {
         App::new()
@@ -86,7 +129,7 @@ async fn main() -> std::io::Result<()> {
                 client: Client::new(),
             }))
             .wrap(Logger::default())
-            .service(user)
+            .service(get_subreddit)
             .service(index)
     })
     .bind("0.0.0.0:8080")?
